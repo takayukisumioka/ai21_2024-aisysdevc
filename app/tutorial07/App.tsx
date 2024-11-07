e5b15d0dc2cde421bca96522233b963e13adef06
takayukisumioka <takayuki_sumioka@yse-c.net>
2024-11-07 05:38:54 +0000

    +2ボタン追加

--- app/tutorial07/App.tsx
+++ app/tutorial07/App.tsx
@@ -9,13 +9,18 @@ export const App = () => {
         setNum(num + 1)
     }
 
+    const onClickButton2 = () => {
+        setNum(num + 1)
+        setNum(num + 1)
+    }
+
     return (
         <>
             <h1 style={{color: "red"}}>こんにちは！</h1>
             <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
             <ColoredMessage color="pink">元気です！</ColoredMessage>
             <button onClick={onClickButton1}>+1</button>
-            <button onClick={() => {alert("ボタン2")}}>ボタン2</button>
+            <button onClick={onClickButton2}>+2</button>
             <p>{num}</p>
         </>
     )
