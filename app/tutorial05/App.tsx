1b10954c662d5ffbe1bc0556fe9e052c1f2c6755
takayukisumioka <takayuki_sumioka@yse-c.net>
2024-11-07 01:34:17 +0000

    ColoredMessageの追加

--- app/tutorial05/App.tsx
+++ app/tutorial05/App.tsx
@@ -10,6 +10,7 @@ export const App = () => {
         <>
             <h1 style={{color: "red"}}>こんにちは！</h1>
             <ColoredMessage color="blue" message="お元気ですか？" />
+            <ColoredMessage color="pink" message="元気です！" />
             <button onClick={onClickButton}>ボタン</button>
             <button onClick={() => {alert("ボタン2")}}>ボタン2</button>
         </>
